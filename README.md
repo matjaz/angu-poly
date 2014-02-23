ba82a349f861da3bae320056154d608353912ecd
Matjaz Lipus <matjazl@gmail.com>
2014-02-23 18:23:09 +0100

    init

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-angu-poly
+Angu-poly
 =========
 
-AngularJS &amp; Polymer two way binding.
+AngularJS & Polymer two way binding
+-----------------------------------
+
+
+Polyglot for AngularJS and Polymer a simple two way binding of attribute values, using, Node.bind and MutationObserver.
+
+JavaScript implementation inspired by [Angular and Polymer Data Binding, Together!](http://blog.sethladd.com/2014/02/angular-and-polymer-data-binding.html).
---
+++ angupoly.js
@@ -0,0 +1,54 @@
+angular.module('angupoly', [])
+.directive('angupoly', function($rootScope, $parse) {
+  return {
+    priority : 42,
+    restrict : 'A',
+    compile  : function(tElement, tAttr) {
+      var conf = $rootScope.$eval(tAttr.angupoly);
+      var assignables = {};
+      var paths       = {};
+      var needsMutationObserver;
+
+      Object.keys(conf).forEach(function(attrName) {
+        var path  = conf[attrName],
+            parse = $parse(path);
+        if (parse.assign) {
+          assignables[attrName] = parse.assign;
+          needsMutationObserver = true;
+        }
+        paths[attrName] = path;
+      });
+      return function(scope, element) {
+        var el = element[0];
+
+        // from angular scope to attribute
+        // http://www.polymer-project.org/platform/node_bind.html
+        for (var attrName in paths) {
+          el.bind(attrName, new PathObserver(scope, paths[attrName]));
+        }
+
+        if (needsMutationObserver) {
+          // from attribute to angular scope
+          // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+          // http://caniuse.com/mutationobserver
+          new MutationObserver(function(mutations) {
+            var mutation,
+                updates,
+                i = 0;
+            while ((mutation = mutations[i++])) {
+              for (var attrName in assignables) {
+                if (mutation.attributeName.indexOf(attrName) !== -1) {
+                  assignables[attrName](scope, mutation.target[attrName]);
+                  updates = true;
+                }
+              }
+            }
+            if (updates) {
+              scope.$apply();
+            }
+          }).observe(el, {attributes:true});
+        }
+      };
+    }
+  };
+});
